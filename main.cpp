1bce97a1299da95329703249ae9a256b1d0abebe
domimik <domimikmisztal@gmail.com>
2021-02-05 12:27:31 +0100

    Zmiany dodatkowe w 03

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int policz(int a, int b)
+{
+    return (a+b)*(a-b);
+}
+
+int main()
+{
+    std::cout<<"Hello, World!"<<std::endl;
+    std::cout<<"W nowym branchu; 2+3="<<policz(2,3)<<std::endl;
     return 0;
 }
